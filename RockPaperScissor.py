2bb44dd75c97b457c0e40d4daf70844d2652dcd4
Matthew Vieyra <carratos@yahoo.com>
2017-05-13 10:34:14 -0700

    Create RockPaperScissor.py
    Create main file and add comment

---
+++ RockPaperScissor.py
@@ -0,0 +1,4 @@
+'''
+Rock Paper Scissor Game against the computer. User enters their choice of rock, paper, or scissor and computer randomly
+chooses a response and result are output to the console.
+'''
